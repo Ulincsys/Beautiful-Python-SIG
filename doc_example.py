4a42f6badd0617e752b32ed9bcde52fad500d1cc
Ulincsys <ulincsys@gmail.com>
2023-11-28 16:30:23 -0600

    Add docstring example

---
+++ doc_example.py
@@ -0,0 +1,21 @@
+def mystery_function(a, b):
+    """
+    This function adds the two parameters together.
+
+    It *will* throw an exception if either parameter
+    is None, and also if there is no way to add them
+    together.
+
+    Parameters
+    ----------
+    a : Any
+        The addend
+    b : Any
+        the augend
+
+    Returns
+    -------
+    Any
+        The result of a + b
+    """
+    return a + b
